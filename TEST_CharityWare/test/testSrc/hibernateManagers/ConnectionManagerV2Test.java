2bcec323e8db5db09773dadc2eddb9345726f962
Alex Martin <a.martin.12@ucl.ac.uk>
2013-02-07 10:56:57 +0000

    modifications to test

--- TEST_CharityWare/test/testSrc/hibernateManagers/ConnectionManagerV2Test.java
+++ TEST_CharityWare/test/testSrc/hibernateManagers/ConnectionManagerV2Test.java
@@ -5,21 +5,29 @@ package hibernateManagers;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import hibernateEntities.*;
 
 /**
  * @author Alex
  *
  */
 public class ConnectionManagerV2Test {
-
+	
+	public ConnectionManagerV2 TestManager;
+	
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
+		TestManager = new ConnectionManagerV2();
 	}
 
 	/**
@@ -30,8 +38,26 @@ public class ConnectionManagerV2Test {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	/**
+	 * Test that the getter and setter methods are working.
+	 */
+	public void testGetSet() {
+		String ConfName = "hibernate.cfg.xml";
+		TestManager.setDBConfname("hibernate.cfg.xml");		
+		assertEquals("Getter and Setter working",ConfName,TestManager.getDBConfname());
 	}
+	
+	@Test
+	/**
+	 * Test that the get table statement is working.
+	 */
+	public void testGetTable() {
+		TestManager.setDBConfname("hibernate.cfg.xml");	
+		ArrayList<User> Alex = (ArrayList<User>)TestManager.getTable("User");
+		assertNotNull("Get Table Working",Alex);
+	}
+		
+
+	
 
 }
